fd4fe4dd669a534cb30b8c0ec00ce4da0bbd9217
Andrew Leonard <andy@hurricane-ridge.com>
2013-08-14 20:15:58 -0700

    Silence puppet-lint.

--- vagrant/vagrant.pp
+++ vagrant/vagrant.pp
@@ -1,5 +1,9 @@
 # Include this module
 class { 'iptables_persistent':
-  limit_out => [ { 'port' => 25, 'rate' => '1/minute', 'burst' => 2 } ],
+  limit_out => [ {
+    'port'  => 25,
+    'rate'  => '1/minute',
+    'burst' => 2
+  } ],
   permit_in => [ 22, 80, 443 ],
 }
